f47d734646c3d73f93cb7f16c3eeca549c210922
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2024-12-14 20:19:51 +0900

    feature: 강남 데이터 전달 api
    1. /map을 통해 지도 시각화에 사용할 데이터 전달 (지도, 툴팁,
       도로/시간별)
    
    2. /graph를 통해 특정 도로의 시간별 통행량, 시간별 속도 변화량 데이터
       전달

---
+++ domain/gangnam/data_processor.py
@@ -0,0 +1,60 @@
+import pandas as pd
+
+
+road_name_to_file = {
+    "도로없음": "static/data/data.csv",
+    "강남대로": "static/data/gangnam.csv",
+    "논현로": "static/data/nonhyeon.csv",
+    "도곡로": "static/data/dogok.csv",
+    "도산로":"static/data/dosan.csv",
+    "봉은사로":"static/data/bongeunsa.csv",
+    "삼성로":"static/data/samsung.csv",
+    "선릉로":"static/data/seolleung.csv",
+    "압구정로":"static/data/apgujeong.csv",
+    "역삼로":"static/data/yeoksam.csv",
+    "영동대로":"static/data/yeongdong.csv",
+    "온주로":"static/data/eonju.csv",
+    "테헤란로":"static/data/teheran.csv",
+    "학동로":"static/data/hakdong.csv"
+}
+
+
+def get_traffic_data(interval, road_name):
+
+    data = pd.read_csv(road_name_to_file.get(road_name))
+
+    data.fillna(0, inplace=True)
+
+    data["traffic_volume"] = data["entered"] + data["departed"]
+
+    interval = int(interval)
+    # 시간 구간 필터링
+    filtered_data = data[data["interval"] == interval]
+
+    # 시간별 그룹화하여 통행량 계산
+    response = (
+        filtered_data.groupby(["interval", "id"], as_index=False)
+        .agg({
+            "traffic_volume": "sum",
+            "speed": "mean"
+        })
+        .sort_values(by=["interval", "id"])
+    )
+    return response
+
+def get_data_with_hours(road_name,road_ids):
+    data = pd.read_csv(road_name_to_file.get(road_name))
+    data.fillna(0, inplace=True)
+    data["traffic_volume"] = data["entered"] + data["departed"]
+    filtered_data = data[data["id"].isin(road_ids)]
+
+    grouped_data = (
+        filtered_data
+        .groupby("interval", as_index=False)
+        .agg({
+            "traffic_volume": "sum",
+            "speed": "mean"
+        })
+        .sort_values(by="interval")
+    )
+    return grouped_data
